e2f753dbf31e8f80d75a61a644677a4f5e1a8645
dibicono <dibicono@gmail.com>
2023-09-26 21:32:27 -0400

    segundo dia continuo

--- asset/index.js
+++ asset/index.js
@@ -1,16 +1,15 @@
-console.log("Entro index.js");
 
 let contactos = JSON.parse( localStorage.getItem("contactos") ) || [];
 // Estos son las referencias a mis inputs
 const NOMBRE = document.getElementById("NOMBRE");
 const APELLIDO = document.getElementById("APELLIDO");
-const CELULAR = document.getElementById("CELULAR");
+const TELEFONO = document.getElementById("TELEFONO");
 const ANOTACIONES = document.getElementById("ANOTACIONES");
-
 
 // Estas son las referencias a mis botones
 const btnBORRAR = document.getElementById("btnBORRAR");
 const btnEDITAR = document.getElementById("btnEDITAR");
+const btnINGRESAR = document.getElementById("btnINGRESAR")
 
 const divPeliculas = document.getElementById("divPeliculas");
 const alertSinPeliculas = document.getElementById("alertSinPeliculas");
@@ -18,36 +17,32 @@ const alertSinPeliculas = document.getElementById("alertSinPeliculas");
 let indexEditar = null;
 
 
-class Pelicula {
-    constructor(titulo, estreno, genero, imagen, sinopsis) {
-        this.titulo = titulo;
-        this.estreno = estreno;
-        this.genero = genero;
-        this.imagen = imagen;
-        this.sinopsis = sinopsis;
+class DIRECTORIO {
+    constructor(NOMBRE, APELLIDO, TELEFONO, ANOTACIONES) {
+        this.NOMBRE = NOMBRE;
+        this.APELLIDO = APELLIDO;
+        this.TELEFONO = TELEFONO;
+        this.ANOTACIONES = ANOTACIONES;
     }
 }
 
+function guardarContacto() {
+    let NOMBRE = NOMBRE.value;
+    let APELLIDO = APELLIDO.value;
+    let TELEFONO = TELEFONO.value;
+    let ANOTACIONES = ANOTACIONES.value;
 
-function guardarPelicula() {
-    let titulo = inputTitulo.value;
-    let estreno = inputEstreno.value;
-    let genero = inputGenero.value;
-    let imagen = inputImagen.value;
-    let sinopsis = inputSinopsis.value;
-
-    let pelicula = new Pelicula(
-        titulo,
-        estreno,
-        genero,
-        imagen,
-        sinopsis
+    let guardarContacto = new DIRECTORIO(
+        NOMBRE,
+        APELLIDO,
+        TELEFONO,
+        ANOTACIONES,
     );
-    console.log(pelicula);
+    
 
     if (indexEditar === null) {
         console.log("Agregar pelicula");
-        peliculas.push(pelicula);
+        guardarContacto.push(DIRECTORIO);
     } else {
         console.log("Editar pelicula");
     }
@@ -60,12 +55,12 @@ function borrarTodo() {
     mostrarPeliculas();
 }
 
-function mostrarPeliculas() {
+function mostrarDIRETORIO() {
     console.log("Deberia mostrar peliculas....");
 }
 
 
-btnAgregar.addEventListener("click", guardarPelicula);
-btnBorrarTodo.addEventListener("click", borrarTodo);
+btnINGRESAR.addEventListener("click", guardarContacto);
+btnBORRAR.addEventListener("click", borrarTodo);
 
 mostrarPeliculas();
